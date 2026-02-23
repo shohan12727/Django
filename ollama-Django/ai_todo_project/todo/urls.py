6a27df2973ebc215a4fcbcea2078005feb76172a
shohan12727 <aishohan001@gmail.com>
2026-02-24 02:32:52 +0600

    templete fixed

--- ollama-Django/ai_todo_project/todo/urls.py
+++ ollama-Django/ai_todo_project/todo/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
-from . import views 
+from . import views
 
+urlpatterns = [
+    path("", views.index, name="index"),
+    # path("add/", views.add_todo, name="add_todo"),
+    # path("delete/<uuid:todo_id>", views.delete_todo, name="delete_todo"),
+    # path("completer/<uuid:todo_id>", views.complete_todo, name="complete_todo"),
+]
