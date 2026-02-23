3ed7e4995c8aef34cf984e3c7ce9ffb75e3fb973
shohan12727 <aishohan001@gmail.com>
2026-02-23 15:53:35 +0600

    project end

--- Django-littlelemon/littlelemon/restaurant/views.py
+++ Django-littlelemon/littlelemon/restaurant/views.py
@@ -26,3 +26,11 @@ def book(request):
 def menu(request):
     menu_data = Menu.objects.all()
     return render(request, 'menu.html', {'menu': menu_data})
+
+def display_menu_item(request, pk=None):
+    if pk:
+        menu_item = Menu.objects.get(pk=pk)
+    else:
+        menu_item = ""    
+    
+    return render(request, 'menu_item.html',{"menu_item": menu_item})
