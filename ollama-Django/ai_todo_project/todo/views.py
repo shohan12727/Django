8611ccfd6714922950fb6d550c51f46ea7a3d9b8
shohan12727 <aishohan001@gmail.com>
2026-02-24 12:45:57 +0600

    add added

--- ollama-Django/ai_todo_project/todo/views.py
+++ ollama-Django/ai_todo_project/todo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import TodoItem
+from .forms import TodoItemForm
+import uuid
+from django.shortcuts import render, redirect
 # Create your views here.
 
 def index(request):
@@ -7,3 +10,24 @@ def index(request):
     return render(request, "todo/index.html", {"todo_items": todo_items})
 
 
+def add_todo(request):
+    if request.method == "GET":
+        form = TodoItemForm()
+        return render(request,"todo/add_todo.html",{"form":form})
+    elif request.method == "POST":
+        form = TodoItemForm(request.POST)
+        
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            description = form.cleaned_data["description"]
+            
+            
+            TodoItem.objects.create(
+                id = uuid.uuid4(),
+                title = title,
+                description = description 
+            )
+            
+        return redirect("index")    
+        
+        
