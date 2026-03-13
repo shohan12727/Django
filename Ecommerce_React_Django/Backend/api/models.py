910a5b01e061781eb3b2bf4c1f876833e16cf7ec
shohan12727 <aishohan001@gmail.com>
2026-03-14 01:31:27 +0600

    product and category model added

--- Ecommerce_React_Django/Backend/api/models.py
+++ Ecommerce_React_Django/Backend/api/models.py
@@ -1,8 +1,30 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
+    category = models.ForeignKey(
+        Category, related_name="products", on_delete=models.CASCADE
+    )
     name = models.CharField(max_length=100)
-    price = models.FloatField()
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
+    
