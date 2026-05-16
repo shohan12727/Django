7cfd053b166a8415f6e64b2726c2effac5ac9b9b
shohan12727 <aishohan001@gmail.com>
2026-05-16 12:49:55 +0600

    product model created

--- A-Trash-Management/authBackend/product/models.py
+++ A-Trash-Management/authBackend/product/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=20)
+    description = models.CharField(max_length=255, blank=True, null=True)
+
+    price = models.DecimalField(max_digits=10)
+    stock_quantity = models.IntegerField(default=0)
+    category = models.CharField(max_length=30)
+    is_available = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
