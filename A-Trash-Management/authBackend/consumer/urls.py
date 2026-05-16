09342a682ae5fb43b023d85142634898a7707513
shohan12727 <aishohan001@gmail.com>
2026-05-16 12:38:24 +0600

    updated

--- A-Trash-Management/authBackend/consumer/urls.py
+++ A-Trash-Management/authBackend/consumer/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import ConsumerView
 
 urlpatterns = [
-    
+    path("consumers/", ConsumerView.as_view())
 ]
--- A-Trash-Management/authBackend/consumer/views.py
+++ A-Trash-Management/authBackend/consumer/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Consumer
+from .serializers import ConsumerSerializer
 
 # Create your views here.
+
+
+class ConsumerView(APIView):
+
+    def get(self, request):
+        consumers = Consumer.objects.all().order_by("-created_at")
+        serializers = ConsumerSerializer(consumers, many=True)
+        return Response(serializers.data)
+
+    def post(self, request):
+
+        serializers = ConsumerSerializer(data=request.data)
+
+        if serializers.is_valid():
+            serializers.save()
+            return Response("Consumer created successfully")
+
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
