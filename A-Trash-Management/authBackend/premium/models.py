db44f05cc3e85349d019b0eb4ba61d885905475a
shohan12727 <aishohan001@gmail.com>
2026-05-17 11:45:02 +0600

    premium model added

--- A-Trash-Management/authBackend/premium/models.py
+++ A-Trash-Management/authBackend/premium/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Premium(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    BILLING_CYCLE_CHOICES = [
+        ("monthly", "monthly"),
+        ("yearly", "yearly"),
+        ("lifeTime", "lifeTime"),
+    ]
+    bailing_cycle = models.CharField(
+        max_length=15, choices=BILLING_CYCLE_CHOICES, default="monthly"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.name}"
