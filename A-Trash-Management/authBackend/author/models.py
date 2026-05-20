4edc7f32a69851e989778cc1be29dc18d3285a03
shohan12727 <aishohan001@gmail.com>
2026-05-20 13:45:26 +0600

    post and author added

--- A-Trash-Management/authBackend/author/models.py
+++ A-Trash-Management/authBackend/author/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class AuthorModel(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- A-Trash-Management/authBackend/posts/models.py
+++ A-Trash-Management/authBackend/posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from author.models import AuthorModel
 
 # Create your models here.
+
+
+class PostModel(models.Model):
+    author = models.ForeignKey(
+        AuthorModel, on_delete=models.CASCADE, related_name="posts"
+    )
+    title = models.CharField(max_length=40)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
