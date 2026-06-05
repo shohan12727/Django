9495d8e27516ab1c6708fc65079d3ee399dcc732
shohan12727 <aishohan001@gmail.com>
2026-06-05 22:16:46 +0600

    account created

---
+++ AIS/backend/accounts/views.py
@@ -0,0 +1,50 @@
+# accounts/views.py
+
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated
+)
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import (
+    RegisterSerializer,
+    UserSerializer
+)
+
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+
+        serializer = RegisterSerializer(
+            data=request.data
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+
+        serializer = UserSerializer(
+            request.user
+        )
+
+        return Response(serializer.data)
--- AIS/backend/core_backend/settings.py
+++ AIS/backend/core_backend/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "accounts",
+
 ]
 
 MIDDLEWARE = [
@@ -120,3 +124,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# REST Framework Configuration
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
